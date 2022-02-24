681ba2e334b4738db9cb1d0eb377024dfd819405
twosugar <sugarfish@yeah.net>
2022-02-24 18:15:38 +0800

    feat: add puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+export { default as openBrowser } from './openBrowser'
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('boop3');
   }
   return a + b;
 };
---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: 
+ * @Author: ytang5
+ * @Date: 2022-02-24 16:44:16
+ * @LastEditors: ytang5
+ * @FilePath: /easy-skeleton/src/types/index.d.ts
+ * @LastEditTime: 2022-02-24 16:51:52
+ */
